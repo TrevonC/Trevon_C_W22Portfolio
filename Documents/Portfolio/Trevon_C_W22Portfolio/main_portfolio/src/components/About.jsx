84d963bb2c8921f72715c413cbe839636e95ee65
TrevonC <Trevon.Costa@georgebrown.ca>
2022-04-07 02:52:41 -0400

    about me - progress

--- Documents/Portfolio/Trevon_C_W22Portfolio/main_portfolio/src/components/About.jsx
+++ Documents/Portfolio/Trevon_C_W22Portfolio/main_portfolio/src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill, BsFillArrowUpCircleFill, BsFillStopCircleFill } from 'react-icons/bs';
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill, BsFillArrowUpCircleFill, BsFillStopCircleFill, BsFillArrowDownCircleFill } from 'react-icons/bs';
 import { MdNotStarted } from 'react-icons/md';
 
 const About = () => {
@@ -11,42 +11,42 @@ const About = () => {
             <ol class="items-center sm:flex max-w-[1000px] py-4">
                 <li class="relative mb-6 sm:mb-0 ">
                     <div class="flex items-center">
-                    <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-red-400">
-                            <BsFillStopCircleFill size={50} className=""/>
+                    <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-green-400">
+                            <MdNotStarted size={50} className=""/>
                         </div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                     </div>
                     <div class="mt-3 sm:pr-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.0.0</h3>
-                        <time class="block mb-2 text-sm font-normal leading-none text-indigo-900">Released on December 2, 2021</time>
-                        <p class="text-base font-normal text-[#08192f]">Get started with dozens of web components and interactive elements.</p>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Started Programming Journey</h3>
+                        <time class="block mb-2 text-sm font-normal leading-none text-indigo-900">April, 2019</time>
+                        <p class="text-base font-normal text-[#08192f]">Started making small fun and interactive projects using Python.</p>
                     </div>
                 </li>
                 <li class="relative mb-6 sm:mb-0">
                     <div class="flex items-center">
                         <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-gray-200">
-                            <BsFillArrowLeftCircleFill size={50} className=""/>
+                            <BsFillArrowRightCircleFill size={50} className=""/>
                         </div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                     </div>
                     <div class="mt-3 sm:pr-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.0</h3>
-                        <time class="block mb-2 text-sm font-normal leading-none text-indigo-900">Released on December 23, 2021</time>
-                        <p class="text-base font-normal text-[#08192f]">Get started with dozens of web components and interactive elements.</p>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Enrolled into George Brown College</h3>
+                        <time class="block mb-2 text-sm font-normal leading-none text-indigo-900">December, 2019</time>
+                        <p class="text-base font-normal text-[#08192f]">Joined the Computer Programming and Analysis Program offered at George Brown College.</p>
                     </div>
                 </li>
                 <li class="relative mb-6 sm:mb-0">
                     <div class="flex items-center">
                     <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-gray-200">
-                            <BsFillArrowLeftCircleFill size={50} className=""/>
+                            <BsFillArrowDownCircleFill size={50} className=""/>
                         </div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                     </div>
                     <div class="mt-3 sm:pr-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.3.0</h3>
-                        <time class="block mb-2 text-sm font-normal leading-none text-indigo-900">Released on January 5, 2022</time>
-                        <p class="text-base font-normal text-[#08192f]">Get started with dozens of web components and interactive elements.</p>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Started my First Semester</h3>
+                        <time class="block mb-2 text-sm font-normal leading-none text-indigo-900">January, 2020</time>
+                        <p class="text-base font-normal text-[#08192f]">Studied at the George Brown Campus where I learned the fundamentals.</p>
                     </div>
                 </li>
             </ol>
@@ -54,21 +54,21 @@ const About = () => {
                 <li class="relative mb-6 sm:mb-0">
                     <div class="flex items-center">
                     <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-gray-200">
-                            <BsFillArrowRightCircleFill size={50} className=""/>
+                            <BsFillArrowDownCircleFill size={50} className=""/>
                         </div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                     </div>
                     <div class="mt-3 sm:pr-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Semester 2, Remote Learning</h3>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Semester Four </h3>
                         <time class="block mb-2 text-sm font-normal leading-none text-indigo-900">April, 2020</time>
-                        <p class="text-base font-normal text-[#08192f]">With COVID-19 taking over the world, we had to make a transition to Online.</p>
+                        <p class="text-base font-normal text-[#08192f]">Faced many challenging courses such as Data Structures, Agile, Python. </p>
                     </div>
                 </li>
                 <li class="relative mb-6 sm:mb-0">
                     <div class="flex items-center">
                     <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-yellow-400">
-                            <BsFillArrowRightCircleFill size={50} className=""/>
+                            <BsFillArrowLeftCircleFill size={50} className=""/>
                         </div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
@@ -82,15 +82,15 @@ const About = () => {
                 <li class="relative mb-6 sm:mb-0">
                     <div class="flex items-center">
                     <div class="flex z-10 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-gray-200">
-                            <BsFillArrowUpCircleFill size={50} className=""/>
+                            <BsFillArrowLeftCircleFill size={50} className=""/>
                         </div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                     </div>
                     <div class="mt-3 sm:pr-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Semester Four</h3>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Semester 2, Remote Learning</h3>
                         <time class="block mb-2 text-sm font-normal leading-none text-indigo-900">January, 2021</time>
-                        <p class="text-base font-normal text-[#08192f]">. Highlights: Data Structures, Agile, Python. </p>
+                        <p class="text-base font-normal text-[#08192f]">With COVID-19 taking over the world, we had to make a transition to Online.</p>
                     </div>
                 </li>
             </ol>
@@ -98,21 +98,21 @@ const About = () => {
                 <li class="relative mb-6 sm:mb-0">
                     <div class="flex items-center">
                     <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-gray-200">
-                            <BsFillArrowUpCircleFill size={50} className=""/>
+                            <BsFillArrowRightCircleFill size={50} className=""/>
                         </div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                     </div>
                     <div class="mt-3 sm:pr-8">
-                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Started my First Semester</h3>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Semester 5, Collaboration</h3>
                         <time class="block mb-2 text-sm font-normal leading-none text-indigo-900">January, 2020</time>
-                        <p class="text-base font-normal text-[#08192f]">Studied at the George Brown Campus where I learned the fundamentals.</p>
+                        <p class="text-base font-normal text-[#08192f]">Collaborated with many peers to build small scale application projects.</p>
                     </div>
                 </li>
                 <li class="relative mb-6 sm:mb-0">
                     <div class="flex items-center">
                     <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-gray-200">
-                            <BsFillArrowLeftCircleFill size={50} className=""/>
+                            <BsFillArrowRightCircleFill size={50} className=""/>
                         </div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
@@ -125,8 +125,8 @@ const About = () => {
                 </li>
                 <li class="relative mb-6 sm:mb-0">
                     <div class="flex items-center">
-                    <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-green-400">
-                            <MdNotStarted size={50} className=""/>
+                    <div class="flex justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0 text-red-400">
+                            <BsFillStopCircleFill size={50} className=""/>
                         </div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                         <div class="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
--- Documents/Portfolio/Trevon_C_W22Portfolio/main_portfolio/src/components/Contact.jsx
+++ Documents/Portfolio/Trevon_C_W22Portfolio/main_portfolio/src/components/Contact.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const Contact = () => {
     return (
         <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4 bg-gradient-to-t from-[#08192f] to-blue-400'>
-        <form method='POST' action="https://getform.io/f/0092124e-7066-4b1e-bf0b-e1d0790b393a" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-white text-[#08192f]'>Contact</p>
-                <p className='text-gray-300 py-4'>Feel free to fill out this form to send me a message with any inquiry or if you'd like to work with me!
-                You can also reach me here: Trevonccg@hotmail.com</p>
-            </div>
-            <input className='bg-[#ededed] p-2' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ededed]' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ededed] p-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 px-6 py-3 my-2 flex items-center bg-green-500 
+            <form method='POST' action="https://getform.io/f/0092124e-7066-4b1e-bf0b-e1d0790b393a" className='flex flex-col max-w-[600px] w-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-white text-[#08192f]'>Contact</p>
+                    <p className='text-gray-300 py-4'>Feel free to fill out this form to send me a message with any inquiry or if you'd like to work with me!
+                        You can also reach me here: Trevonccg@hotmail.com</p>
+                </div>
+                <input className='bg-[#ededed] p-2' type="text" placeholder='Name' name='name' />
+                <input className='my-4 p-2 bg-[#ededed]' type="email" placeholder='Email' name='email' />
+                <textarea className='bg-[#ededed] p-2' name="message" rows="10" placeholder='Message'></textarea>
+                <button className='text-white border-2 px-6 py-3 my-2 flex items-center bg-green-500 
                         border-green-500 hover:bg-red-500 hover:border-red-500 animate-pulse ease-out transform hover:scale-125 transition duration-500'>Let's connect!</button>
-        </form>
-    </div>
+            </form>
+        </div>
     )
 }
 
